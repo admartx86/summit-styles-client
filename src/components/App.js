0abd08df7b287b14463a67c855fb7517333908ff
Adam Martinez <12470793+mart3318@users.noreply.github.com>
2023-08-07 12:37:20 -0500

    feat: Add page components and routing

--- src/components/App.js
+++ src/components/App.js
@@ -1,10 +1,33 @@
 import React, { Component } from "react";
-
+import Shop from "./Shop";
+import MyAccount from "./MyAccount";
+import Wishlist from './Wishlist';
+import ShoppingCart from "./ShoppingCart";
+import About from "./About";
+import Home  from "./Home";
+import { Route, Routes, Link } from "react-router-dom";
 class App extends Component {
   render() {
     return (
       <div>
-        <h1>Hello world!</h1>
+        <nav>
+        <Link to="/">Home</Link>
+        <Link to="/shop">Shop</Link>
+        <Link to="/account">My Account</Link>
+        <Link to="/wishlist">Wishlist</Link>
+        <Link to="/cart">Shopping Cart</Link>
+        <Link to="/about">About</Link>
+        </nav>
+
+        <Routes>
+          <Route path="/" element={<Home />} />
+          <Route path="/shop" element={<Shop />} />
+          <Route path="/account" element={<MyAccount />} />
+          <Route path="/wishlist" element={<Wishlist />} />
+          <Route path="/cart" element={<ShoppingCart />} />
+          <Route path="/about" element={<About />} />
+        </Routes>
+
       </div>
     );
   }
