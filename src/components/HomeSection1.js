839f682e811caf04a275d07e073aa4bfcbefbaa8
Adam Martinez <12470793+mart3318@users.noreply.github.com>
2023-08-09 13:09:36 -0500

    feat: Add HomeSection1 and HomeSection3

---
+++ src/components/HomeSection1.js
@@ -0,0 +1,11 @@
+import React from 'react';
+
+const HomeSection1 = () => {
+    return(
+        <div>
+            Fall Sale! Save up to 30%! <button>View Collection</button>
+        </div>
+    )
+}
+
+export default HomeSection1;
---
+++ src/components/HomeSection3.js
@@ -0,0 +1,11 @@
+import React from "react";
+
+const HomeSection3 = () => {
+    return(
+        <div>
+            New Arrivals for Fall 2023 <button>View Collection</button>
+        </div>
+    )
+};
+
+export default HomeSection3;
