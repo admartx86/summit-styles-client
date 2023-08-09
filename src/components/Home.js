d8729150b4d0f49ee9a81402e3995ddd0a476e69
Adam Martinez <12470793+mart3318@users.noreply.github.com>
2023-08-09 13:09:36 -0500

    feat: Add HomeSection1 and HomeSection3

--- src/components/Home.js
+++ src/components/Home.js
@@ -1,9 +1,13 @@
 import React from 'react';
+import HomeSection1 from './HomeSection1';
+import HomeSection3 from './HomeSection3';
 
 const Home = () => {
     return(
         <div>
             <h1>Home</h1>
+            <HomeSection1 />
+            <HomeSection3 />
         </div>
     )
 }
