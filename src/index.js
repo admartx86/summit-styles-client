73c8ebb08d483df537b72f4a200ba51be0387023
Adam Martinez <12470793+mart3318@users.noreply.github.com>
2023-08-10 18:47:55 -0500

    fix: Remove non-working Switzer ttf, woff, and woff2 font and replace with otf, now working

--- src/index.js
+++ src/index.js
@@ -1,11 +1,14 @@
 import React from "react";
 import ReactDOM from "react-dom";
+import { createRoot } from 'react-dom/client';
 import App from "./components/App";
 import { BrowserRouter } from 'react-router-dom';
 
-ReactDOM.render(
-    <BrowserRouter>
-        <App />
-    </BrowserRouter>,
-    document.getElementById("root")
+const rootElement = document.getElementById('root');
+const root = createRoot(rootElement);
+
+root.render(
+  <BrowserRouter>
+    <App />
+  </BrowserRouter>
 );
