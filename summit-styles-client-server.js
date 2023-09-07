9aef155b85bf7f946bfa7a9b5bbd7c284700a7a2
Adam Martinez <12470793+mart3318@users.noreply.github.com>
2023-09-06 23:39:41 -0500

    build: Remove dist and add dist to .gitignore

--- summit-styles-client-server.js
+++ summit-styles-client-server.js
@@ -1,16 +1,15 @@
-const express = require('express');
-const path = require('path');
+require('dotenv').config();
 
+const express = require('express');
 const app = express();
 
-// Serving static files from "dist" directory
+const path = require('path');
 app.use(express.static(path.join(__dirname, 'dist')));
 
-// Fallback to index.html for single-page applications
 app.get('*', (req, res) => {
   res.sendFile(path.join(__dirname, 'dist', 'index.html'));
 });
 
 app.listen(3001, () => {
-  console.log('Server running on http://localhost:3001/');
+  console.log('app listening on port 3001 (HTTP)');
 });
--- webpack.config.js
+++ webpack.config.js
@@ -1,5 +1,6 @@
 const path = require('path');
 const HTMLWebpackPlugin = require('html-webpack-plugin');
+const Dotenv = require('dotenv-webpack');
 
 module.exports = {
 
@@ -13,7 +14,8 @@ module.exports = {
     plugins: [
         new HTMLWebpackPlugin({
             template: './src/index.html'
-        })
+        }),
+        new Dotenv()
     ],
 
     module: {
