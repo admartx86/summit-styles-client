58cbb9bd52f1edea98707b79c3d3772edd9190c1
Adam Martinez <12470793+mart3318@users.noreply.github.com>
2023-09-10 01:38:48 -0500

    feat: Add responsive header

--- src/components/SiteLogo.js
+++ src/components/SiteLogo.js
@@ -4,7 +4,7 @@ import { Link } from 'react-router-dom';
 const SiteLogo = () => {
     return (
         <Link className='site-logo' to='/'>
-            <img src='https://summit-styles.s3.us-east-2.amazonaws.com/summit-styles-logo.png' alt='Summit Styles Logo' />
+            <img src='https://summit-styles.s3.us-east-2.amazonaws.com/summit-styles-logo-533-100-px.png' alt='home' />
         </Link>
     );
 }
