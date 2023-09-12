dee894bc4c6b07e5711130e83c7da485fb3a4613
Adam Martinez <12470793+mart3318@users.noreply.github.com>
2023-09-11 21:25:41 -0500

    feat: Add responsiveness to header, footer, Section1, Section2, and shop

--- src/components/HomeSection1.js
+++ src/components/HomeSection1.js
@@ -4,7 +4,10 @@ import backgroundImage from '../img/a.png';
 
 const HomeSection1 = () => {
     return(
-        <section className='Section1' style={{ backgroundImage: `url(${backgroundImage})` }}>
+         <section className='Section1' style={{ 
+            backgroundImage: `url(${backgroundImage})`, 
+            backgroundPosition: 'center',
+            }}>
             <p>New Styles<br></br>for Fall 2023</p> 
             <Link to="/shop/new-featured">
                 <button>View Collection</button>
--- src/components/MainNavigation.js
+++ src/components/MainNavigation.js
@@ -5,13 +5,13 @@ import HamburgerMenu from './HamburgerMenu';
 const MainNavigation = () => {
     return(
         <nav className="main-navigation">
-            <HamburgerMenu />
         <Link to="/" className="main-navigation-link" ><img src="https://summit-styles.s3.us-east-2.amazonaws.com/home-32-px.png" alt=''/><p>Home</p></Link>
         <Link to="/shop" className="main-navigation-link" ><img src="https://summit-styles.s3.us-east-2.amazonaws.com/shop-32-px.png" alt=''/><p>Shop</p></Link>
         <Link to="/wishlist" className="main-navigation-link" ><img src="https://summit-styles.s3.us-east-2.amazonaws.com/heart-32-px.png" alt='' /><p>Favorites</p></Link>
         <Link to="/cart" className="main-navigation-link" ><img src="https://summit-styles.s3.us-east-2.amazonaws.com/cart-32-px.png" alt='' /><p>Cart</p></Link>
         <Link to="/account" className="main-navigation-link" ><img src="https://summit-styles.s3.us-east-2.amazonaws.com/user-32-px.png" alt='' /><p>My Account</p></Link>
         {/*<Link to="/about">About</Link>*/}
+        <HamburgerMenu />
         </nav>
     )
 }
