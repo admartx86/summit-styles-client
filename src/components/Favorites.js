e81ae2064528ebddddeb28223f6e54f17e83244b
Adam Martinez <12470793+mart3318@users.noreply.github.com>
2023-09-19 22:16:35 -0500

    fix: Fix issue where remove from favorites button on Favorites page does not rerender page

--- src/components/Favorites.js
+++ src/components/Favorites.js
@@ -1,31 +1,18 @@
-import React, { useState, useEffect } from 'react';
+import React, { useEffect } from 'react';
 import axios from 'axios';
-import { onRemoveFromFavorites } from './RemoveFromFavorites';
 import { Link, useNavigate } from 'react-router-dom';
+import useProductState from './useProductState'; // Import the custom hook
 
 const Favorites = () => {
-  // Define state to hold favorite items
-  const [favoriteItems, setFavoriteItems] = useState([]);
+  const { 
+    favoriteItems, // Fetch favorite items from the hook
+    handleRemoveFromFavorites // Use the function to remove items from favorites
+  } = useProductState(null);
 
-
-  
-
-  
   // Fetch favorite items from the backend when the component mounts
+  // This logic is already in the custom hook, so you can remove it from here
   useEffect(() => {
-    const fetchFavorites = async () => {
-      try {
-        const response = await axios.get(`${process.env.REACT_APP_BACKEND_URL}/get-favorites`, {
-          withCredentials: true,
-        });
-        if (response.status === 200) {
-          setFavoriteItems(response.data.favorites);
-        }
-      } catch (error) {
-        console.error('Failed to fetch favorites:', error);
-      }
-    };
-    fetchFavorites();
+    // You can use your custom hook here for additional setup if necessary.
   }, []);
 
   return (
@@ -36,21 +23,17 @@ const Favorites = () => {
       ) : (
         <div>
           {Array.isArray(favoriteItems) && favoriteItems.map((item, index) => (
-            console.log('Current Item:', item),
             <div className="favorite-item" key={index}>
               <Link to={`/shop/product/${item.id}`}>
-              <div className="link-to-favorite-item">
-              
-              <img src={item.image} alt={item.name} width="50" height="50" />
-              <div>{item.name}</div>
-              <div>{`$${item.price}`}</div>
-              </div>
+                <div className="link-to-favorite-item">
+                  <img src={item.image} alt={item.name} width="50" height="50" />
+                  <div>{item.name}</div>
+                  <div>{`$${item.price}`}</div>
+                </div>
               </Link>
               <button onClick={() => {
-                onRemoveFromFavorites({productId: item.id});
-                }}>Remove
-                </button>
-
+                handleRemoveFromFavorites({productId: item.id}); // Use the function from the custom hook
+              }}>Remove</button>
             </div>
           ))}
         </div>
@@ -60,3 +43,4 @@ const Favorites = () => {
 };
 
 export default Favorites;
+
