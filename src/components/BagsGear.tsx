5b8040e1fd38485f355f0503b9197928493c35f5
Adam Martinez <12470793+mart3318@users.noreply.github.com>
2024-02-28 19:39:28 -0600

    refactor: Add types to BagsGear

--- src/components/BagsGear.tsx
+++ src/components/BagsGear.tsx
@@ -1,7 +1,7 @@
 import React from 'react';
 import ProductList from './ProductList';
 
-const BagsGear = () => {
+const BagsGear: React.FC = () => {
   return (
     <div>
       <h1 className="shop-title">Bags & Gear</h1>
