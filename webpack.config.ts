e4df33a342d6638c8b9d471b2b9c2058263b2c17
Adam Martinez <12470793+mart3318@users.noreply.github.com>
2024-02-29 13:32:22 -0600

    fix: resolve issue where favicon did not render

--- webpack.config.ts
+++ webpack.config.ts
@@ -13,7 +13,8 @@ module.exports = {
 
   plugins: [
     new HTMLWebpackPlugin({
-      template: './src/index.html'
+      template: './src/index.html',
+      favicon: './src/favicon.ico',
     }),
     new Dotenv()
   ],
